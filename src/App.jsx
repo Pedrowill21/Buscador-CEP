03af111da3dd1f6d6daaa3e86dbf309364c446bf
Pedrowill21 <pedrowilliam2999@gmail.com>
2023-11-11 17:50:10 -0300

    build

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
  
   return (
     <div className=" bg-slate-950 h-full min-h-screen flex flex-col items-center justify-center text-orange-500">
-      <h1 className=' font-bold text-orange-500 text-4xl '>Buscador de Cep</h1>
+      <h1 className=' font-bold text-orange-500 text-4xl '>Buscador de CEP</h1>
       <Buscador/>
 
     </div>
